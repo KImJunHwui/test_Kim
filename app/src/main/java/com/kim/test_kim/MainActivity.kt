a89cc7d75724892a6319d81bfc713bd574aaa357
KJH <daeding7@naver.com>
2021-11-17 20:30:51 +0900

    실제로 저장 / 저장된 값 조회 기능 활용

--- app/src/main/java/com/kim/test_kim/MainActivity.kt
+++ app/src/main/java/com/kim/test_kim/MainActivity.kt
@@ -2,10 +2,25 @@ package com.kim.test_kim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.internal.ContextUtils
+import com.kim.test_kim.utility.utility
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+
+    autoLoginCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        utility.setAutoLogin(this, isChecked)
+
 }
+       autoLoginCheckBox.isChecked = utility.getAutoLogin(this)
+
+       }
+
+
+        }
+
--- app/src/main/java/com/kim/test_kim/utility/utility.kt
+++ app/src/main/java/com/kim/test_kim/utility/utility.kt
@@ -25,4 +25,5 @@ class utility {
     }
 
 
+
 }
